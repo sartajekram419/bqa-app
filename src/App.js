4a1d19ff0bb18eb69cfc6f3911e7612833db0afc
Syed Mohammed Sartaj Ekram <sartajekram419@gmail.com>
2022-05-12 22:55:11 +0600

    Connected to sagorsarker/mbert-bengali-tydiqa-qa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Form } from './Components'
+import { Form , Div, Button } from './Components'
+import fetch from "node-fetch";
+
+
+//ENDPOINT = https://api-inference.huggingface.co/models/<sagorsarker/mbert-bengali-tydiqa-qa>
+//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
 
 
 class App extends Component {
@@ -10,19 +15,41 @@ class App extends Component {
 
     this.state = {
 
+      model: null,
+
       question: "",
       passage: "",
+      answer: "ssssssssssssssssssssssssssfsfssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+
+      shown: false,
 
       styleHeading: {
           color: "#fff",
           textAlign: "center",
       },
       styleInput: {
-          height: "40px",
+          height: "20px",
           padding: "0px 0px 0px 10px",
+          background: "#EAEAF5",
+          
+          borderRadius: "5px 5px 5px 5px",
+          border: "2px solid #EAEAF5"
       },
+      styleInput1: {
+        height: "100px",
+        padding: "0px 0px 0px 10px",
+        background: "#EAEAF5",
+      
+        borderRadius: "5px 5px 5px 5px",
+        border: "2px solid #EAEAF5",
+        justifyContent: "start",
+        textAlignVertical: 'top',
+        textAlign: 'top'
+    },
       styleLabel: {
           padding: "0px 0px 0px 0px",
+          font: '200px',
+          textAlign: 'left'
       },
       styleHr: {
           background: "transparent",
@@ -37,13 +64,50 @@ class App extends Component {
 
     this.setQuestion = this.setQuestion.bind(this);
     this.setPassage = this.setPassage.bind(this);
-
+    this.setShown = this.setShown.bind(this);
+    this.setModel = this.setModel.bind(this);
+    this.setAnswer = this.setAnswer.bind(this);
   }
 
-  computePressed = event => {
+  computePressed = async event => {
     event.preventDefault();
 
+    /*query(this.state.question, this.state.passage).then((response) => {
+      this.setAnswer(JSON.stringify(response));
+    });*/
+
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/sagorsarker/mbert-bengali-tydiqa-qa",
+      {
+          headers: { Authorization: `Bearer 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` },
+          method: "POST",
+          body: JSON.stringify({'question': this.state.question, 'context': this.state.passage}),
+      }
+  );
+  const result = await response.json();
+  this.setAnswer(JSON.stringify(result["answer"]));
+
+    this.setShown();
   };
+
+  /*query = async (data) => {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/sagorsarker/mbert-bengali-tydiqa-qa",
+        {
+            headers: { Authorization: `Bearer ${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}` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+  };*/
+
+  setModel(data) {
+    this.setState({
+        model: data,
+    })
+  }
 
   setPassage(data) {
     this.setState({
@@ -57,11 +121,22 @@ class App extends Component {
     })
   }
 
+  setAnswer(data) {
+    this.setState({
+        answer: data,
+    })
+  }
+
+  setShown() {
+    this.setState({
+      shown: true,
+    })
+  } 
 
 
   render() {
     return (
-      <div>
+      <div className='App'>
         <header className="App-header">
         Bangla Question Anserwing
         </header>
@@ -71,7 +146,7 @@ class App extends Component {
             Enter the Question
           </label>
           <hr style={this.state.styleHr}></hr>
-          <input onChange={(e) => { this.setQuestion(e.target.value) }} type="text" placeholder="Question" />
+          <input style={this.state.styleInput} onChange={(e) => { this.setQuestion(e.target.value) }} type="text" />
   
           <hr style={this.state.styleHr}></hr>
           <hr style={this.state.styleHr}></hr>
@@ -81,7 +156,7 @@ class App extends Component {
             Enter the Context
           </label>
           <hr style={this.state.styleHr}></hr>
-          <input onChange={(e) => { this.setPassage(e.target.value) }} type="text" placeholder="Context" />
+          <input style={this.state.styleInput1} onChange={(e) => { this.setPassage(e.target.value) }} type="text" />
 
 
           <hr style={this.state.styleHr}></hr>
@@ -90,8 +165,23 @@ class App extends Component {
           <hr style={this.state.styleHr}></hr>
           <hr style={this.state.styleHr}></hr>
           <hr style={this.state.styleHr}></hr>
-          <input type="submit" value="Compute" />
         </Form>
+
+        <Button onClick={this.computePressed}>Compute</Button>
+
+        {this.state.shown &&
+          <label style={this.state.styleLabel}>
+            Answer
+          </label>
+        }
+
+        {this.state.shown &&
+          <Div>
+            <text>
+            {this.state.answer}
+            </text>
+          </Div>
+        }
   
       </div>
     );
--- src/Components.js
+++ src/Components.js
@@ -15,3 +15,41 @@ export const Form = styled.form`
         width: 70%;
     }
 `;
+
+export const Div = styled.div`
+    background: #EAEAF5;
+    display: flex;
+    padding: 3px 0px 0px 0px;
+    justify-content: start;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin: auto;
+    width: 55%;
+    @media (max-width: 768px) {
+        flex-direction: column;
+        width: 70%;
+    }
+    overflow: scroll;
+    border-radius: 5px 5px 5px 5px;
+    border: 2px solid #EAEAF5;
+`;
+
+export const Button = styled.button`
+    background: #1ce636;
+    
+    align-items: center;
+    justify-content: center;
+    
+    color: white;
+    width: 100%;
+    max-width: 150px;
+    height: 40px;
+    @media (max-width: 768px) {
+        width: 35%;
+    }
+    margin-top: 0px;
+    margin-bottom: 20px;
+    cursor: pointer;
+    border-radius:10px 10px 10px 10px;
+    border: 2px solid #1ce636;
+`;
